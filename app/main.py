f7b799969c3586a2f7c959a1e18735f931070a20
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-02-03 13:46:13 +0200

    Solution to implementing car wash logic service

--- app/main.py
+++ app/main.py
@@ -1,8 +1,36 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        income = 0
+        for car in cars:
+            if self.clean_power > car.clean_mark:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        pollution_diff = abs(car.clean_mark - self.clean_power)
+        rating_factor = self.average_rating / self.distance_from_city_center
+        return round(car.comfort_class * pollution_diff * rating_factor, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int) -> None:
+        ratings_sum = self.count_of_ratings * self.average_rating + rate
+        self.count_of_ratings += 1
+        self.average_rating = round(ratings_sum / self.count_of_ratings, 1)
